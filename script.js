bae4d03febc5ea0daed5d8d6cadf31ee3205052d
minamoto <minamotoorin@gmail.com>
2022-05-31 23:03:55 +0800

    Adding mode switcher.

--- script.js
+++ script.js
@@ -12,11 +12,41 @@ const cover = document.getElementById('cover');
 const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
+//生成从minNum到maxNum的随机数
+function randomNum(minNum,maxNum){ 
+  switch(arguments.length){ 
+      case 1: 
+          return parseInt(Math.random()*minNum+1,10); 
+      break; 
+      case 2: 
+          return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+      break; 
+          default: 
+              return 0; 
+          break; 
+  } 
+}
+
 class Player {
   constructor() {
     this.songs     = ['hey', 'summer', 'ukulele'];
     this.songIndex = 0;
-    this.mode      = 0;
+
+    this.modelist2func  = 
+    { 
+      "repeat":()=>{return 0},
+      "circle":()=>{return 1},
+      "random":()=>{return randomNum(1, this.songs.length - 1)}
+    };
+    this.modelist2icon  = 
+    { 
+      "repeat":["fa", "fa-music"],
+      "circle":["fa", "fa-rotate-right"],
+      "random":["fa", "fa-gratipay"]
+    };
+    this.modelist  = Object.keys(this.modelist2func);
+    this.mode      = 1;
+    
     this.loadSong()
   }
 
@@ -44,9 +74,9 @@ class Player {
 
   next()
   {
-    this.songIndex++;
-    if (this.songIndex > this.songs.length - 1) {
-      this.songIndex = 0;
+    this.songIndex += this.modelist2func[this.modelist[this.mode]]();
+    if (this.songIndex >= this.songs.length) {
+      this.songIndex %= this.songs.length;
     }
 
     this.loadSong();
@@ -56,8 +86,10 @@ class Player {
   // Play song
   playSong() {
     musicContainer.classList.add('play');
-    playBtn.querySelector('i.fa').classList.remove('fa-play');
-    playBtn.querySelector('i.fa').classList.add('fa-pause');
+    
+    x = document.getElementById('x')
+    x.classList.remove('fa-play');
+    x.classList.add('fa-pause');
   
     audio.play();
   }
@@ -65,10 +97,11 @@ class Player {
   // Pause song
   pauseSong() {
     musicContainer.classList.remove('play');
-  //   playBtn.querySelector('i.fa').classList.add('fa-play');
-  //   playBtn.querySelector('i.fa').classList.remove('fa-pause');
-    document.getElementById('x').classList.add('fa-play');
-    document.getElementById('x').classList.remove('fa-pause');
+    // playBtn.querySelector('i.fa').classList.add('fa-play');
+    // playBtn.querySelector('i.fa').classList.remove('fa-pause');
+    x = document.getElementById('x')
+    x.classList.add('fa-play');
+    x.classList.remove('fa-pause');
   
     audio.pause();
   }
@@ -76,6 +109,13 @@ class Player {
   switchMode()
   {
     this.mode ++;
+    if (this.mode >= this.modelist.length) {
+      this.mode = 0;
+    }
+
+    mode_icon = document.getElementById('mode_icon')
+    mode_icon.classList = "";
+    mode_icon.classList.add(...this.modelist2icon[this.modelist[this.mode]]);
   }
 }
 
